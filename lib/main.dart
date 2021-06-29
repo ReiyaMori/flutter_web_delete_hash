d7abc73ee194513a6b3d0231fa58de8b0193eca0
ReiyaMori <hanedab787@gmail.com>
2021-06-30 01:39:31 +0900

    ＃削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  // ここに追加
+  setPathUrlStrategy();
   runApp(MyApp());
 }
 
+// 以下同じ
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       title: '＃消すサンプル',
       debugShowCheckedModeBanner: false,
@@ -19,7 +22,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: Center(child: Text('🧸', style: TextStyle(fontSize: 30),),),
     );

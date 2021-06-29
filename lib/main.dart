8d5f8ea8a43d618490de64b6c8144b1e647a6f15
ReiyaMori <hanedab787@gmail.com>
2021-06-30 01:24:02 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: '＃消すサンプル',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(child: Text('🧸', style: TextStyle(fontSize: 30),),),
+    );
+  }
+}
